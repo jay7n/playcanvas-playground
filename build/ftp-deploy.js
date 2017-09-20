7027ae54157ed1e08cffd472cc6f7b18050233bf
lookof <lookof@gmail.com>
2017-09-20 09:40:58 +0800

    add 'deploy' build script to automate ftp deploying process

---
+++ build/ftp-deploy.js
@@ -0,0 +1,44 @@
+var path = require('path')
+var ora = require('ora')
+var chalk = require('chalk')
+var FtpDeploy = require('ftp-deploy')
+
+function deploy(spinner) {
+    var localRoot = path.join(__dirname, '..', 'dist')
+    var remoteRoot = '/htdocs/playcanvas_demo/'
+    var host = 'hz226350.ftp.aliapp.com'
+    var ftpDeploy = new FtpDeploy()
+
+    var config = {
+        host,
+        localRoot,
+        remoteRoot,
+        username: "hz226350",
+        password: "RayIon2016", // optional, prompted if none given
+        port: 21,
+        exclude: ['.git', '.idea', 'tmp/*', 'build/*', '.DS_Store']
+    }
+
+    spinner.start(`deploying: ${chalk.green(localRoot)} --> ${chalk.green(host)}: ${chalk.green(remoteRoot)}`)
+
+    return new Promise((resolve, reject) => {
+        ftpDeploy.deploy(config, function(err) {
+            if (err)  reject(err)
+            else resolve()
+        })
+    })
+}
+
+async function main() {
+    var spinner = ora()
+    try {
+        await deploy(spinner)
+        spinner.succeed()
+    } catch (err) {
+        spinner.fail(chalk.red(err))
+    } finally {
+        spinner.stop()
+    }
+}
+
+main()
