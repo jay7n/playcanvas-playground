def52fe79f6f97540549cd4b8510b9e864ffc2a7
lookof <lookof@gmail.com>
2017-09-20 12:51:58 +0800

    improve build for production script more convenient

--- build/prod.js
+++ build/prod.js
@@ -1,15 +1,16 @@
-var ora = require('ora')
-var webpack = require('webpack')
-var path = require('path')
-var chalk = require('chalk')
+const ora = require('ora')
+const webpack = require('webpack')
+const path = require('path')
+const chalk = require('chalk')
+const opn = require('opn')
+const parseArgs = require('minimist')
+const _ = require('lodash')
 
-var Conf = require('../config/conf')
+const Conf = require('../config/conf')
 
-var webpackConfig = require('../config/webpack/prod.conf')
+const webpackConfig = require('../config/webpack/prod.conf')
 
-var spinner = ora('building for production...')
-
-function _build_prod(spinner) {
+function _build_prod() {
     return new Promise((resolve, reject) => {
         webpack(webpackConfig, function(err, stats) {
             if (err) reject(err)
@@ -27,18 +28,48 @@ function _build_prod(spinner) {
     })
 }
 
-async function main() {
+function _start_server(port) {
+    return new Promise((resolve, reject) => {
+        const express = require('express')
+        const app = express()
+        app.use('/', express.static(path.resolve(Conf.RootPath, 'dist')))
+        app.listen(port, function(err) {
+            if (err) reject(err)
+            opn('http://127.0.0.1:8087')
+            resolve()
+        })
+    })
+}
+
+async function main(args) {
     try {
-        console.log(chalk.green('build production...'))
-        await _build_prod()
-        console.log(chalk.green('build production done.'))
+        let has_build_arg = _.includes(args._, 'build')
+        let has_server_arg = _.includes(args._, 'server')
+
+        if (_.isEmpty(args._)) {
+            has_build_arg = true
+            has_server_arg = true
+        }
+
+        if (has_build_arg) {
+            console.log(chalk.green('build production...'))
+            await _build_prod()
+            console.log(chalk.green('build production done.'))
+        }
+        if (has_server_arg) {
+            const port = args.port || 8087
+            await _start_server(port)
+            console.log(chalk.green(`start server listening at port ${port} ... `))
+        }
+
     } catch(err) {
         console.log(chalk.red(err))
     }
 }
 
 if (require.main === module) {
-    main()
+    const args = parseArgs(process.argv.slice(2))
+    main(args)
 } else {
     module.exports = main
 }
